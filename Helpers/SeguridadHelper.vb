a5ff868d50980506fc048c83817cba6c286fab44
Danielaflores-12 <danielafloresquincho@gmail.com>
2026-09-15 11:57:36 -0500

    subida de la carpeta helper

---
+++ Helpers/SeguridadHelper.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Security.Cryptography
+Imports System.Text
+
+Public Class SeguridadHelper
+
+    Public Shared Function EncriptarSHA256(texto As String) As String
+
+        Using sha As SHA256 = SHA256.Create()
+
+            Dim bytes As Byte() =
+                Encoding.UTF8.GetBytes(texto)
+
+            Dim hash As Byte() =
+                sha.ComputeHash(bytes)
+
+            Dim resultado As New StringBuilder()
+
+            For Each b As Byte In hash
+                resultado.Append(b.ToString("x2"))
+            Next
+
+            Return resultado.ToString()
+
+        End Using
+
+    End Function
+
+End Class
---
+++ Helpers/ValidacionHelper.vb
@@ -0,0 +1,13 @@
+﻿Public Class ValidacionHelper
+
+    Public Shared Function TextoVacio(valor As String) As Boolean
+        Return String.IsNullOrWhiteSpace(valor)
+    End Function
+
+    Public Shared Function TelefonoValido(telefono As String) As Boolean
+        Return telefono IsNot Nothing AndAlso
+               telefono.Length = 9 AndAlso
+               IsNumeric(telefono)
+    End Function
+
+End Class
